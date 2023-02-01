62386daaa333715beecc338e44dcad68aa00bdb4
vhee4 <vivianafogu@gmail.com>
2023-02-01 12:44:22 +0100

    implemented welcome page

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // SCROLL ANIMATION
 
 const boxes = document.querySelectorAll(".box");
+const menuBar = document.querySelector(".menu-wrap");
+const containerWrapper = document.getElementById("showcase");
+
+const removeContainer = () => {
+  containerWrapper.classList.toggle("none")
+}
+
+menuBar.addEventListener("click", removeContainer)
+
+
 
 window.addEventListener("scroll", checkBoxes);
 
