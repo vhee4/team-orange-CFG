57533eb37b413ba642b94bc755acb20a9e98a73a
Stella Bampou <stellabampou@gmail.com>
2023-02-03 14:35:56 +0200

    Created new page for Signup and login forms

--- form.js
+++ form.js
@@ -9,3 +9,14 @@ document.querySelector("#open-subscribe-form").addEventListener("click",function
   document.getElementById("show-message").addEventListener("click", function() {
     alert("Thank you for subscribing");
   });
+
+  // Get the modal for Sign up and Login forms
+var modal1 = document.getElementById('id01');
+var modal2 = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
