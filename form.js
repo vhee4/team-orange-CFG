7e147ad9bed88441a6844c387be6eff8955628bd
Stella Bampou <stellabampou@gmail.com>
2023-02-04 00:19:35 +0200

    Modified Signup and Login forms - Signup.html, form-styles.css

--- form.js
+++ form.js
@@ -16,7 +16,14 @@ var modal2 = document.getElementById('id02');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+    
+  }
+}
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+    
   }
 }
