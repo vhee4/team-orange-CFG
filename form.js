51984edcf18c1faa3d1f3bf6fd22bd769e31fff8
Stella Bampou <stellabampou@gmail.com>
2023-02-04 10:13:33 +0200

    Changes in signup and login buttons

--- form.js
+++ form.js
@@ -1,29 +1,25 @@
-document.querySelector("#open-subscribe-form").addEventListener("click",function(){
+document
+  .querySelector("#open-subscribe-form")
+  .addEventListener("click", function () {
     document.querySelector(".subscribe-form").classList.add("active");
   });
-  
-  document.querySelector(".subscribe-form .close-btn").addEventListener("click",function(){
+
+document
+  .querySelector(".subscribe-form .close-btn")
+  .addEventListener("click", function () {
     document.querySelector(".subscribe-form").classList.remove("active");
   });
 
-  document.getElementById("show-message").addEventListener("click", function() {
-    alert("Thank you for subscribing");
-  });
+// Display thank you for subscribing message
+document.getElementById("show-message").addEventListener("click", function () {
+  alert("Thank you for subscribing");
+});
 
-  // Get the modal for Sign up and Login forms
-var modal1 = document.getElementById('id01');
-var modal2 = document.getElementById('id02');
+// Get the modal for Sign up and Login forms
+var modal1 = document.getElementById("id01");
+var modal2 = document.getElementById("id02");
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal1) {
-    modal1.style.display = "none";
-    
-  }
-}
-window.onclick = function(event) {
-  if (event.target == modal2) {
-    modal2.style.display = "none";
-    
-  }
-}
+// Display "thank you for signing up" message
+document.getElementById("signup").addEventListener("click", function () {
+  alert("Thank you for Signing up");
+});
