4a227bc8395eed87be1690d2c65590679da3056a
Stella Bampou <stellabampou@gmail.com>
2023-02-04 11:16:52 +0200

    Completed the Signup page, added subscribe button in the footer section

--- form.js
+++ form.js
@@ -1,3 +1,43 @@
+// SCROLL ANIMATION
+
+const boxes = document.querySelectorAll(".box");
+const menuBar = document.querySelector(".menu-wrap");
+const contentWrapper = document.getElementById("content");
+
+const removeContent = () => {
+  contentWrapper.classList.toggle("none")
+}
+
+menuBar.addEventListener("click", removeContent)
+
+//the explore button on the welcome page which will direct to the home page 
+ function myFunction() {
+      window.location.href = "Home.html";
+    }
+
+
+
+window.addEventListener("scroll", checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+  boxes.forEach((box, idx) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
+
+
+
+
+
 document
   .querySelector("#open-subscribe-form")
   .addEventListener("click", function () {
