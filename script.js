0627d1ef0cba6a8db35e983fcd769f441fca90f2
vhee4 <vivianafogu@gmail.com>
2023-02-06 12:40:10 +0100

    arranged the files

--- script.js
+++ script.js
@@ -48,3 +48,18 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
+
+//THE SUBSCRIBE BUTTON
+
+document.querySelector("#open-subscribe-form").addEventListener("click",function(){
+    document.querySelector(".subscribe-form").classList.add("active");
+  });
+  
+  document.querySelector(".subscribe-form .close-btn").addEventListener("click",function(){
+    document.querySelector(".subscribe-form").classList.remove("active");
+  });
+
+  document.getElementById("show-message").addEventListener("click", function() {
+    alert("Thank you for subscribing");
+  });
+
